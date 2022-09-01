92f6dae61275bad71ef79fb9adef214a7483269e
Shelumiel <sssharon7@gmail.com>
2022-09-01 13:48:57 +0800

    Add Search Params

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
 
 function Users() {
+  const [searchParam, setSearchParam] = useSearchParams();
+  const showActiveUsers = searchParam.get("filter") === "active";
   return (
     <div>
       <nav>
@@ -10,6 +12,17 @@ function Users() {
         <Link to="admin">Admin</Link>
       </nav>
       <Outlet />
+      <div>
+        <button onClick={() => setSearchParam({ filter: "active" })}>
+          Active users
+        </button>
+        <button onClick={() => setSearchParam({})}>Reset users</button>
+      </div>
+      {showActiveUsers ? (
+        <h2>Showing active users</h2>
+      ) : (
+        <h2>Showing all users</h2>
+      )}
     </div>
   );
 }
